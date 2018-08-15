5569039eac2f88365b82919e4650bf3deef49868
Rob j Loranger <robjloranger@protonmail.com>
2018-08-15 09:50:21 -0700

    shouldersTemplate: fix typo (#3)
    In this context `altogether` should be `all together`.

--- shoulders/shoulders.go
+++ shoulders/shoulders.go
@@ -79,7 +79,7 @@ func DepList() ([]string, error) {
 var shouldersTemplate = `
 # {{.Name}} Stands on the Shoulders of Giants
 
-{{.Name}} does not try to reinvent the wheel! Instead, it uses the already great wheels developed by the Go community and puts them altogether in the best way possible. Without these giants this project would not be possible. Please make sure to check them out and thank them for all of their hard work.
+{{.Name}} does not try to reinvent the wheel! Instead, it uses the already great wheels developed by the Go community and puts them all together in the best way possible. Without these giants this project would not be possible. Please make sure to check them out and thank them for all of their hard work.
 
 Thank you to the following **GIANTS**:
 
